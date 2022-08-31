2b14b631055a99d14a28e229401af7ebccd0618a
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-08-31 23:55:05 +0530

    added fucntionality for button and the hint is added dynamically

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const inputs = document.querySelector(".inputs");
+const inputs = document.querySelector(".inputs"),
+  resetBtn = document.querySelector(".reset-btn"),
+  hint = document.querySelector(".hint span");
 function randomWord() {
   //getting random object from wordList
   let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
   let word = ranObj.word; //getting word from random object
   console.log(word);
-
+  hint.innerText = ranObj.hint;
   let html = "";
   for (let i = 0; i < word.length; i++) {
     html += `<input type="text" disabled>`;
@@ -12,3 +14,5 @@ function randomWord() {
   inputs.innerHTML = html;
 }
 randomWord();
+
+resetBtn.addEventListener("click", randomWord);
